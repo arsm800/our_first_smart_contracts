fba58d012ff6bd7be88c7167757b3145fa1da99a
Andrew Smith-Mui <asmithmui@gmail.com>
2020-11-09 20:29:24 -0500

    Created employee address variables.

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.5.0;
 // lvl 1: equal split
 contract AssociateProfitSplitter {
     // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
 
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
@@ -16,7 +19,7 @@ contract AssociateProfitSplitter {
 
     function deposit() public payable {
         // @TODO: Split `msg.value` into three
-        uint amount = ; // Your code here!
+        //uint amount = ; // Your code here!
 
         // @TODO: Transfer the amount to each employee
         // Your code here!
