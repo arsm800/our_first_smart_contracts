985ad9d2cc4ae46f7ccdeb06c9358aedbf1150f6
Andrew Smith-Mui <asmithmui@gmail.com>
2020-11-13 17:34:20 -0500

    Updated all 'now' variables to 'fakenow' to test timelock functionality.:

--- DeferredEquityPlan.sol
+++ DeferredEquityPlan.sol
@@ -15,10 +15,10 @@ contract DeferredEquityPlan {
     uint total_shares = 1000;
     uint annual_distribution = 250;
 
-    uint start_time = now; // permanently store the time this contract was initialized
+    uint start_time = fakenow; // permanently store the time this contract was initialized
 
     // Set the `unlock_time` to be 365 days from now
-    uint unlock_time = now + 365 days;
+    uint unlock_time = fakenow + 365 days;
 
     uint public distributed_shares; // starts at 0
 
@@ -33,7 +33,7 @@ contract DeferredEquityPlan {
 
         // Add "require" statements to enforce that:
         // 1: `unlock_time` is less than or equal to `now`
-        require(unlock_time <= now, "Account is locked!");
+        require(unlock_time <= fakenow, "Account is locked!");
         
         // 2: `distributed_shares` is less than the `total_shares`
         require(distributed_shares < total_shares, "Error: Distrbuted shares exceeds total shares");
@@ -42,7 +42,7 @@ contract DeferredEquityPlan {
         unlock_time = unlock_time + 365 days;
 
         // Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
-        distributed_shares = (now - start_time) / 365 days * annual_distribution;
+        distributed_shares = (fakenow - start_time) / 365 days * annual_distribution;
 
         // double check in case the employee does not cash out until after 5+ years
         if (distributed_shares > 1000) {
