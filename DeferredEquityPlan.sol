8e85db69cd5250dca6f65816dcf8d7cfbae65d13
Andrew Smith-Mui <asmithmui@gmail.com>
2020-11-13 17:25:46 -0500

    Added 'fastforward' function in 'DeferredEquityPlan' contract to test timelock functionality.

--- DeferredEquityPlan.sol
+++ DeferredEquityPlan.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.5.0;
 
 // lvl 3: equity plan
 contract DeferredEquityPlan {
+    
+    //variable to test timelock functionality
+    uint fakenow = now;
+    
     address human_resources;
 
     address payable employee; // bob
@@ -38,7 +42,6 @@ contract DeferredEquityPlan {
         unlock_time = unlock_time + 365 days;
 
         // Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
-        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
         distributed_shares = (now - start_time) / 365 days * annual_distribution;
 
         // double check in case the employee does not cash out until after 5+ years
@@ -56,5 +59,10 @@ contract DeferredEquityPlan {
     // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
     function() external payable {
         revert("Do not send Ether to this contract!");
+    }
+    
+    //fastforward function to testing the functionality of timelock
+    function fastforward() public {
+        fakenow += 100 days;
     }
 }
